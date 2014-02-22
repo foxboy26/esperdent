547b5eef5e591873f394a9559876f69060e9ef44
Zhiheng Li <lizhiheng.sjtu@gmail.com>
2014-02-22 15:45:08 -0800

    test case for select

---
+++ src/test/java/edu/ucsd/cs/triton/compiler/TestLogicPlanBasicSelect.java
@@ -0,0 +1,55 @@
+package edu.ucsd.cs.triton.compiler;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import parser.ASTStart;
+import parser.ParseException;
+import parser.TritonParser;
+import edu.ucsd.cs.triton.operator.LogicPlan;
+import edu.ucsd.cs.triton.operator.LogicPlanVisitor;
+import edu.ucsd.cs.triton.resources.ResourceManager;
+
+public class TestLogicPlanBasicSelect {
+	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+		
+		String fileName = "src/test/jjtree/select.esp";
+
+		try {
+			TritonParser tritonParser;
+			tritonParser = new TritonParser(new FileInputStream(new File(
+			    fileName)));
+
+			ASTStart root;
+
+			root = tritonParser.Start();
+			// root.dump(">");
+			
+			ResourceManager resourceManager = ResourceManager.getInstance();
+			
+			LogicPlanVisitor logicPlanVisitor = new LogicPlanVisitor(resourceManager);
+			
+			root.childrenAccept(logicPlanVisitor, resourceManager);
+			
+			System.out.println(resourceManager.getStreamByName("s1"));
+			
+			ArrayList<LogicPlan> logicPlanList = logicPlanVisitor.getLogicPlanList();
+			
+			for (LogicPlan logicPlan : logicPlanList) {
+				logicPlan.dump();
+				System.out.println("------------------");
+			}
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+}
