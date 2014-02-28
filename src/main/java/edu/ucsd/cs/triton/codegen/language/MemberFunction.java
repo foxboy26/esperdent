d295197fb2dcf6126842496db13b0ec5b499ba38
Zhiheng Li <lizhiheng.sjtu@gmail.com>
2014-02-27 23:27:17 -0800

    refactor language builder

---
+++ src/main/java/edu/ucsd/cs/triton/codegen/language/MemberFunction.java
@@ -0,0 +1,24 @@
+package edu.ucsd.cs.triton.codegen.language;
+
+public class MemberFunction extends BlockStatement {
+
+	private final String _signature;
+	
+	public MemberFunction(String signature) {
+	  // TODO Auto-generated constructor stub
+		this._signature = signature;
+	}
+
+	@Override
+  protected void translate(int n, LanguageBuilder sb) {
+	  // TODO Auto-generated method stub
+	  sb.indent(n)
+	  	.append(_signature).beginBlock();
+	  
+	  childrenTranslate(n + PrintStyle.INDENT, sb);
+	  
+	  sb.indent(n)
+	  	.endBlock();
+  }
+
+}
