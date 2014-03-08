f97da18c366a1eebf8983e90797bc4c22845bb4f
Zhiheng Li <lizhiheng.sjtu@gmail.com>
2014-03-08 00:25:49 -0800

    put triton into triton-core

---
+++ triton-core/src/main/java/edu/ucsd/cs/triton/window/SlidingWindowQuery.java
@@ -0,0 +1,8 @@
+package edu.ucsd.cs.triton.window;
+
+import storm.trident.state.BaseQueryFunction;
+import storm.trident.tuple.TridentTuple;
+
+public class SlidingWindowQuery extends BaseQueryFunction<BaseSlidingWindow, TridentTuple> {
+
+}
